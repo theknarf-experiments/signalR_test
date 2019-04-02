b0faffc67955f4bd36903a6095f81abb63cdf9ae
Frank Lyder Bredland <fl@nyggen.com>
2019-04-02 19:35:45 +0200

    Initial chat

--- Hubs/ChatHubs.cs
+++ Hubs/ChatHubs.cs
@@ -5,9 +5,9 @@ namespace SignalR.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", message);
         }
     }
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import * as signalR from '@aspnet/signalr';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', messages: []};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.ping = this.ping.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+
   async componentDidMount() {
     const client = new signalR.HubConnectionBuilder()
       .withUrl("/pingpong")
       .build();
     await client.start();
 
+    const clientChat = new signalR.HubConnectionBuilder()
+      .withUrl("/chatHub")
+      .build();
+    await clientChat.start();
+
     const result = await client.invoke('Ping');
     console.log(result);
 
-    await client.stop();
+    clientChat.on('ReceiveMessage', result => {
+      console.log('ReceiveMessage', result)
+      this.setState({
+          messages: [...this.state.messages, result]
+      });
+    });
+
+    this.setState({ client, clientChat })
+  }
+
+  async componentWillUnmount() {
+    console.log('state.client.stop()')
+    await this.state.client.stop();
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  submitMessage(event) {
+    if(event.keyCode == 13) {
+      this.state.clientChat.send('SendMessage', event.target.value);
+      this.setState({ value: '' });
+    }
+  }
+
+  async ping() {
+    const result = await this.state.client.invoke('Ping');
+    console.log('ping:', result);
   }
 
   render() {
     return (
       <div className="App">
-        <input />
+        <input value={this.state.value} onChange={this.handleChange} onKeyUp={this.submitMessage} />
+        <button onClick={this.ping}>Ping</button>
         <div>
-          
+        {
+            (this.state.messages||[]).map( message =>
+              <div> {message} </div>
+            )
+        }
         </div>
       </div>
     );
